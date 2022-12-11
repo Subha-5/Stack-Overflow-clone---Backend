d6421542e2ff6d75af8d8313521454a7b34c7976
Subha-5 <subhasadhu5@gmail.com>
2022-12-12 03:55:54 +0530

    Logout and JWT token security features

--- routes/Answers.js
+++ routes/Answers.js
@@ -1,9 +1,12 @@
 import express from "express";
+
+import auth from '../middlewares/auth.js'
+
 import { postAnswer, deleteAnswer } from '../controllers/Answers.js'
 
 const router = express.Router()
 
-router.patch("/post/:id", postAnswer)
-router.patch('/delete/:id', deleteAnswer)
+router.patch("/post/:id", auth, postAnswer)
+router.patch('/delete/:id', auth, deleteAnswer)
 
 export default router
