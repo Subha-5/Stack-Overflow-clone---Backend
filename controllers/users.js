3aaa5f5b65bf436d7d53528903ed6b20dea4e350
Subha-5 <subhasadhu5@gmail.com>
2022-12-13 22:31:05 +0530

    user profile backend

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,21 @@ export const getAllUsers = async (req, res) => {
         })
         res.status(200).json(allUserDetails)
     } catch (error) {
-        res.status(400).json({ message: error.message })
+        res.status(405).json({ message: error.message })
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    const { id: _id } = req.params
+    const { name, about, tags } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('user not unavailable...')
+    }
+    try {
+        const updatedProfile = await User.findByIdAndUpdate(_id, { $set: { 'name': name, 'about': about, 'tags': tags } }, { new: true })
+        res.status(200).json(updateProfile)
+    } catch (error) {
+        res.status(405).json({message: error.message})
     }
 }
